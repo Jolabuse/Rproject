4a8866ae27de47e4a960c5ec74ad1dea28a9b82a
Jolabuse <56165141+Jolabuse@users.noreply.github.com>
2022-05-25 12:22:59 +0200

    résolution simplex simple

---
+++ simplex.r
@@ -0,0 +1,85 @@
+tbl = read.delim(file.choose(),header = FALSE,sep=' ') #store in a table the input file
+
+addBaseCol <- function(tbl,ind){ #insert a base variable for the ind row at the end of A
+  bCol <-matrix(0,nrow(tbl),1)
+  bCol[ind,1]<-1
+  tbl<-cbind(tbl[,1:(ncol(tbl)-2)],bCol,tbl[,(ncol(tbl)-1):ncol(tbl)])
+}
+
+addInvBaseCol <- function(tbl,ind){ #invert the values of the ind row and insert a base variable for it at the end of A
+  tbl[ind,1:(ncol(tbl)-2)]<- tbl[ind,1:(ncol(tbl)-2)]*-1
+  tbl[ind,ncol(tbl)]<- tbl[ind,ncol(tbl)]*-1
+  bCol <-matrix(0,nrow(tbl),1)
+  bCol[ind,1]<-1
+  tbl<-cbind(tbl[,1:(ncol(tbl)-2)],bCol,tbl[,(ncol(tbl)-1):ncol(tbl)])
+}
+
+
+for (ind in c(1:nrow(tbl))) { #create the table
+  if(tbl[ind,(ncol(tbl)-1)] == -1){
+      tbl <- addBaseCol(tbl,ind)
+  }
+  if(tbl[ind,(ncol(tbl)-1)] == 1){
+    if(tbl[ind,(ncol(tbl))] < 0 ){
+         tbl <- addInvBaseCol(tbl,ind)
+    }
+    else{
+      tbl <- addBaseCol(tbl,ind)
+    }
+  }
+}
+
+tbl<- cbind(tbl[,1:(ncol(tbl)-2)],tbl[,(ncol(tbl))])#erase the inequality column
+tbl[nrow(tbl),ncol(tbl)]<-0 #put the 0 for the Z-0
+names=c()  #rename the col
+for (ind in c(1:(ncol(tbl)-1))) {
+  names<-append(names,paste("x",ind,sep=""))
+}
+names <-append(names,"B")
+colnames(tbl)<-names
+
+print(tbl)
+
+
+
+continueCondition <- function(tbl){ #check if only negative in C
+  C <- tbl[nrow(tbl),1:(ncol(tbl)-1)]
+  for (variable in C) {
+    if(variable>0){
+      return(0)
+    }
+  }
+  return(1)
+}
+
+selection <- function(tbl){ #we select the biggest in C and the min positive in B/C in the selected column
+  C <- tbl[nrow(tbl),1:(ncol(tbl)-1)]
+  B <- tbl[1:(nrow(tbl)-1),ncol(tbl)]
+  maxC <- max(C)
+  maxC <- head(which(C == maxC),1)
+  cMaxC <- tbl[1:(nrow(tbl)-1),maxC]
+  BcMaxC <- B/cMaxC
+  minB <- head(which(BcMaxC == min(BcMaxC[BcMaxC>0])),1)
+  c(minB,maxC)
+}
+
+gauss_pivot<-function(mat,pivotLine,pivotColumn){
+  n = mat[pivotLine,pivotColumn]
+  mat[pivotLine,] = mat[pivotLine,]/n
+  for(i in 1:nrow(mat)){
+    if(pivotLine!=i){
+      mat[i,] = mat[i,]-(mat[i,pivotColumn]*mat[pivotLine,])
+    }
+  }
+  
+  return(mat)
+}
+
+
+while(continueCondition(tbl)==0){
+  slct <- selection(tbl)
+  tbl<-gauss_pivot(tbl,slct[1],slct[2])
+  print(tbl)
+}
+
+
